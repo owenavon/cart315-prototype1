ee7aa0db5bd94435c721ce6a810867afad84e85e
Owen Avon <89999663+owenavon@users.noreply.github.com>
2023-02-14 23:16:01 -0500

    Integrated PickUp and Rotate script
    Need to create UI Script to display the amount of tokens that the character finds. Ensure that Token prefab includes scripts.

---
+++ Assets/Rotator.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour 
+{
+    public float rotationSpeed = 0.3f;
+    public PickerUpper player;
+
+    private bool rotate;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame. Do not assume that every GPU can play back at 60 FPS. Iputs, key presses must be in Update.
+    void Update() {
+    }
+
+    // If you need to make changes in regular intervals
+    void FixedUpdate() {
+        this.GetComponent<Transform>().Rotate(0, rotationSpeed, 0);
+    }
+}
